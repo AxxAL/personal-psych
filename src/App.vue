134e41c4c00dd37f83d507a4d07d4c50373a3171
AxxAL <axxal@protonmail.ch>
2023-09-01 14:56:52 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import { RouterView } from "vue-router";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: {
+    RouterView
+  }
+});
+</script>
+
+<template>
+  <main class="wrapper">
+    <RouterView />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main class="container">
+    <p class="font-psych rainbow-trail" style="color: transparent; font-size: 90px;">Magickal Execution</p>
+  </main>
+</template>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
+
+<style lang="scss">
+$colors: #fff56d, #6ddaff, #ba6dff, #ff6dac;
+$size: 20;
+
+@mixin rainbow-shadow($colors, $offset: 0, $size: $size) {
+  $len: length($colors);
+  $outp: 0px 0px transparent;
+  @for $i from 1 through $size {
+    $ind: floor( max($i + $offset, 1) / $size * length($colors) ) % $len + 1;
+    $col: nth($colors, $ind);
+    $outp: #{$outp}, #{$i}px #{$i}px #{$col};
+  }
+  
+  text-shadow: $outp;
+}
+
+.rainbow-trail {
+  animation: animated-rainbow-shadow 1s infinite;
+}
+
+@keyframes animated-rainbow-shadow {
+  @for $i from 0 to $size + 1 {
+    #{$i / $size * 100}% {
+      @include rainbow-shadow($colors, $i);
+    }
+  }
+}
+</style>
